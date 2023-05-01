ccfba5ad77167564c9db5846bce0de76f5969aee
kasunAththanagoda <kasunaththanagoda@gmail.com>
2023-05-01 19:33:24 +0530

    dashboard complete

---
+++ src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav>
+    <v-toolbar flat app >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>App</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat class="grey lighten-3">
+        <span>sign out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" class="indigo" temporary app>
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-action>
+                <v-icon class="white--text">{{ link.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+                <v-list-item-title class="white--text ">{{ link.text }}</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links:[
+        {icon:'mdi-view-dashboard',text:'Dashboard',route:'/'},
+        {icon:'mdi-folder',text:'My projects',route:'/projects'},
+        {icon:'mdi-account',text:'Team',route:'/team'}
+
+
+    ]
+    };
+  },
+};
+</script>
+
+<style></style>
